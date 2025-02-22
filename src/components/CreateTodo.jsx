cee1305d6192737d6f3c5c7859a1059056020ab3
hriday <alauddinhriday@gmail.com>
2025-02-22 19:40:40 +0600

    Some changes

--- src/components/CreateTodo.jsx
+++ src/components/CreateTodo.jsx
@@ -4,9 +4,9 @@ const CreateTodo = () => {
         <div>
             <div className="w-full flex flex-col gap-2 my-4 bg-slate-800">
                 <label>Title : </label>
-                <input className="w-full p-2 rounded-sm" type="text" />
+                <input className="w-full p-2 rounded-sm" type="text" maxLength={50} />
                 <label>Description : </label>
-                <textarea className="min-h-20 max-h-36 w-full p-2 rounded-sm"></textarea>
+                <textarea className="min-h-20 max-h-36 w-full p-2 rounded-sm" maxLength={200}></textarea>
                 <button className="btn bg-blue-500">Save</button>
             </div>
         </div>
--- src/components/TodoApp/TodoApp.jsx
+++ src/components/TodoApp/TodoApp.jsx
@@ -1,25 +1,41 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link } from "react-router-dom";
+import Todo from "../Todo";
+import InProgress from "../InProgress";
+import Done from "../Done";
 
 const TodoApp = () => {
     return (
-        <div className="w-full md:w-5/6 lg:w-3/6 mx-auto border-2 p-4 rounded-md bg-slate-800">
-            <div className="my-8 md:my-10">
-                <h1 className="text-4xl md:text-5xl text-blue-500 font-bold text-center ">Task Management</h1>
+        <div className="w-full md:w-5/6 lg:w-5/6 mx-auto border-2 p-4 rounded-md bg-slate-800">
+            <div className="my-6">
+                <h1 className="text-4xl md:text-5xl text-blue-500 font-bold text-center">Task Management</h1>
             </div>
 
-            <div className="flex flex-col flex-wrap gap-2 py-1 px-1 rounded-sm">
-                <div className="flex justify-between items-center gap-4 bg-secondary rounded-sm p-1 shadow-md">
+            <div className="w-full flex justify-end gap-2 py-1 px-1 rounded-sm">
+                <div className="flex justify-between items-center gap-2 bg-secondary rounded-sm p-1 shadow-md">
                     <h2 className="text-xl px-4 pl-2">Create a list</h2>
                     <Link to='/todo-app/create-todo' className="btn font-bold px-2 md:px-4 border bg-black">Create</Link>
                 </div>
-                <div className="flex justify-start items-center gap-2">
-                    <Link to='/todo-app' className="btn btn-success font-bold px-2 md:px-4 py-1">To-Do</Link>
-                    <Link to='/todo-app/inprogress' className="btn btn-error font-bold px-2 md:px-4 py-1">In Progress</Link>
-                    <Link to='/todo-app/done' className="btn  btn-info font-bold px-2 md:px-4 py-1">Done</Link>
-                </div>          
             </div>
-            <div className="py-4 px-1">
-                <Outlet></Outlet>
+            <div className="p-1 grid grid-cols-3 gap-2">
+                <div>
+                    <div className="border text-center p-1 rounded-md">
+                        <Link to='/todo-app' className="btn btn-success font-bold px-2 md:px-4 py-1 w-full">To-Do</Link>
+                    </div>
+                    <Todo />
+                </div>
+                <div>
+                    <div className="border text-center p-1 rounded-md">
+                        <Link to='/todo-app/inprogress' className="btn btn-error font-bold px-2 md:px-4 py-1 w-full">In Progress</Link>
+                    </div>
+                    <InProgress />
+                </div>
+                <div>
+                    <div className="border text-center p-1 rounded-md">
+                        <Link to='/todo-app/done' className="btn  btn-info font-bold px-2 md:px-4 py-1 w-full">Done</Link>
+                    </div>
+                    <Done />
+                </div>
+
             </div>
         </div>
     );
